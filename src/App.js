2c9072f71629cba3d70c2a4130fe2e2a1700c51a
Prakriti Saxena <prakriti.saxena@tothenew.com>
2016-12-27 14:22:47 +0530

    'test'

--- src/App.js
+++ src/App.js
@@ -5,13 +5,33 @@ import React from 'react'
 
 export default class App extends React.Component {
   constructor() {
-    super()
+    super();
+
+    this.state = {
+      count : 10
+    }
   }
 
+  increment() {
+    this.setState({count: this.state.count + 1})
+  }
   render() {
+    let users = ['prince', 'manoj'];
+    let firstName = 'Prakriti';
+    let lastName = 'Saxena';
+
+    let fullName = `my name is ${firstName} ... ${lastName}`;
     return (
       <div>
-        Working
+        <p>Count is: {this.state.count}</p>
+        <p>{fullName}</p>
+        {
+          users.map((user) =>  {
+            return <p>{user}</p>
+          })
+        }
+
+        <button onClick={this.increment.bind(this)}>Increment</button>
       </div>
     )
   }
